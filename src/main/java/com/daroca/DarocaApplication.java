c5546305ef5f34740281c70a964a5e65b9bb2f1b
RAPHAEL DA SILVA SANTOS <u23334@lapa17.academico.cotuca.unicamp.br>
2024-05-10 21:39:36 -0300

    first commit

---
+++ src/main/java/com/daroca/DarocaApplication.java
@@ -0,0 +1,13 @@
+package com.daroca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarocaApplication.class, args);
+	}
+
+}
